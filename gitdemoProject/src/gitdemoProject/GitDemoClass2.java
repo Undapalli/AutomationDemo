4636c97981fbf5471c5efda9410b71a9ffdabc40
Undapalli <undapallijyothi@gmail.com>
2023-07-25 13:12:41 +0530

    This extends GitClassDemo1 class

--- gitdemoProject/src/gitdemoProject/GitDemoClass2.java
+++ gitdemoProject/src/gitdemoProject/GitDemoClass2.java
@@ -1,9 +1,12 @@
 package gitdemoProject;
 
-public class GitDemoClass2 {
+public class GitDemoClass2 extends GitDemoClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("git demo class 2");
+		
+		
+		
 	}
 
 }
