823039f893d330a273ce2139f7ed7da197817bd1
Undapalli <undapallijyothi@gmail.com>
2023-07-25 15:55:10 +0530

    one more print statment added inside demo1 class

--- gitdemoProject/src/gitdemoProject/GitDemoClass1.java
+++ gitdemoProject/src/gitdemoProject/GitDemoClass1.java
@@ -4,7 +4,7 @@ public class GitDemoClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("GitDemoclass 1");
-
+System.out.println("Github tutorials");
 	}
 
 	public class extendsGitDemoClass1 {
